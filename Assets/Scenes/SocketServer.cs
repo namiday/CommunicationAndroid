2d4165aff7ae2bbdceea75cab6b2cd8bee3b5794
acollot <dovakh0123@gmail.com>
2024-01-29 10:10:15 +0100

    base appli unity

---
+++ Assets/Scenes/SocketServer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+public class SocketServer : MonoBehaviour
+{
+    //private Server server; // Assuming Server is the class handling the socket logic
+    private TcpListener tcpListener;
+    private Thread tcpListenerThread;
+    private TcpClient connectedTcpClient;
+
+    void Start()
+    {
+        tcpListenerThread = new Thread(new ThreadStart(ListenForIncomingRequests));
+        tcpListenerThread.IsBackground = true;
+        tcpListenerThread.Start();
+        Debug.Log("Server Started");
+    }
+
+
+
+    private void ListenForIncomingRequests()
+    {
+        try
+        {
+            tcpListener = new TcpListener(IPAddress.Any, 8052);
+            tcpListener.Start();
+            Byte[] bytes = new Byte[1024];
+            while (true)
+            {
+                using (connectedTcpClient = tcpListener.AcceptTcpClient())
+                {
+                    using (NetworkStream stream = connectedTcpClient.GetStream())
+                    {
+                        int length;
+                        while ((length = stream.Read(bytes, 0, bytes.Length)) != 0)
+                        {
+                            var incomingData = new byte[length];
+                            Array.Copy(bytes, 0, incomingData, 0, length);
+                            string clientMessage = Encoding.ASCII.GetString(incomingData);
+                            Debug.Log("client message received as: " + clientMessage);
+                        }
+                    }
+                }
+            }
+        }
+        catch (SocketException socketException)
+        {
+            Debug.Log("SocketException " + socketException.ToString());
+        }
+    }
+}
