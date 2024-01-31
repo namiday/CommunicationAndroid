36a90ce63c7a03c0430d7e20f23baec55a81e4f3
acollot <dovakh0123@gmail.com>
2024-01-31 16:22:20 +0100

    ajout http rest

--- Assets/SocketServer.cs
+++ Assets/SocketServer.cs
@@ -1,18 +1,85 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class SimpleHttpServer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private HttpListener listener;
+    private Thread listenerThread;
+
     void Start()
     {
-        
+        listener = new HttpListener();
+        listener.Prefixes.Add("http://*:8080/"); // Listen on port 8080, on all network interfaces
+        listener.AuthenticationSchemes = AuthenticationSchemes.Anonymous;
+
+        listenerThread = new Thread(StartListener);
+        listenerThread.IsBackground = true;
+        listenerThread.Start();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void StartListener()
     {
-        
+        listener.Start();
+        Debug.Log("Server started.");
+
+        while (listener.IsListening)
+        {
+            try
+            {
+                Debug.Log("Listening");
+                var context = listener.GetContext();
+                ProcessRequest(context);
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError($"Error: {ex.Message}");
+            }
+        }
+
+        listener.Close();
+    }
+
+    private void ProcessRequest(HttpListenerContext context)
+    {
+        HttpListenerRequest request = context.Request;
+        string dataReceived = null;
+
+        if (request.HttpMethod == "POST")
+        {
+            using (Stream body = request.InputStream)
+            using (StreamReader reader = new StreamReader(body, request.ContentEncoding))
+            {
+                dataReceived = reader.ReadToEnd();
+            }
+        }
+
+        Debug.Log($"Received request: {request.HttpMethod}, Data: {dataReceived}");
+
+        // Prepare the response
+        string responseString = "<html><body>Hello from Unity!</body></html>";
+        byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+
+        HttpListenerResponse response = context.Response;
+        response.ContentLength64 = buffer.Length;
+        response.OutputStream.Write(buffer, 0, buffer.Length);
+        response.OutputStream.Close();
+    }
+
+    void OnApplicationQuit()
+    {
+        if (listener != null)
+        {
+            listener.Stop();
+            listener.Close();
+        }
+
+        if (listenerThread != null)
+        {
+            listenerThread.Abort();
+        }
     }
 }
